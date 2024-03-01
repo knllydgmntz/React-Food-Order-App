6b0f8ee965d4e3dab6a700f8d629cf10b0147420
knllydgmntz <knllydgmntz@gmail.com>
2024-03-01 20:20:39 +0800

    Handling HTTP loading and Error State

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import Header from "./components/Header.jsx";
+import Cart from "./components/UI/Cart.jsx";
 import Meals from "./components/Meals.jsx";
+import Checkout from "./components/UI/Checkout.jsx";
 import { CartContextProvider } from "./store/CartContext.jsx";
+import { UserProgressContextProvider } from "./store/UserProgressContext.jsx";
 
 function App() {
   return (
-    <CartContextProvider>
-      <Header />
-      <Meals />
-    </CartContextProvider>
+    <UserProgressContextProvider>
+      <CartContextProvider>
+        <Header />
+        <Meals />
+        <Cart />
+        <Checkout />
+      </CartContextProvider>
+    </UserProgressContextProvider>
   );
 }
 
